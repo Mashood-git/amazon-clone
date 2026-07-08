5842ff034404412e9a4919f99c15bac6efebe048
Mashood Ahmed Shariff <mashoodahmedshariff007@gmail.com>
2026-07-08 23:15:57 +0530

    Added cart functionality with local storage and notification

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ searchBtn.addEventListener("click", () => {
         alert("Searching for: " + searchInput.value);
     }
 });
+// CART FEATURE
+
+// CART FEATURE
+
+let cartCount = localStorage.getItem("cartCount");
+
+if (cartCount === null) {
+    cartCount = 0;
+} else {
+    cartCount = Number(cartCount);
+}
+
+const cartNumber = document.getElementById("cart-count");
+cartNumber.textContent = cartCount;
+const addButtons = document.querySelectorAll(".add-cart");
+const notification = document.getElementById("notification");
+
+addButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        cartCount++;
+
+        cartNumber.textContent = cartCount;
+
+        localStorage.setItem("cartCount", cartCount);
+
+        notification.style.display = "block";
+
+        setTimeout(() => {
+            notification.style.display = "none";
+        }, 2000);
+
+    });
+
+});
+
+const clearCart = document.getElementById("clear-cart");
+
+clearCart.addEventListener("click", () => {
+    cartCount = 0;
+    cartNumber.textContent = cartCount;
+    localStorage.setItem("cartCount", cartCount);
+});
